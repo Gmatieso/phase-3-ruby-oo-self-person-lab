3ddf7df7f7389f36c854aa17a79f3f149cdc8f9f
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-28 19:04:50 +0300

    using macro to define our instance variable

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,17 @@
 # your code goes here
+class Person
+    #using macros to define our instance variable 
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        
+    end
+end
+#creating an instance of our class 
+human = Person.new("Wanjiku")
+
+#calling out our name
+puts human.name
+
+
