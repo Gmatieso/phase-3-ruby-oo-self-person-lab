b7ece0aae5a014311c65b6e488ea98b0a3b95474
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-28 19:57:00 +0300

    Three name test passing

--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,21 @@
 # your code goes here
 class Person
     #using macros to define our instance variable 
-    attr_reader :name
+    attr_reader :name, :bank_account
 
     def initialize(name)
         @name = name
         
     end
+
+  
+
 end
 #creating an instance of our class 
 human = Person.new("Wanjiku")
 
-#calling out our name
 puts human.name
+
+
 
 
