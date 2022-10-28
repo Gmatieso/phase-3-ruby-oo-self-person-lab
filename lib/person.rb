3cd6cc991f1eef0d43ce34bf30af36a27f4e5c4f
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-28 20:18:26 +0300

    Created Setter and Getter method for our bank account

--- lib/person.rb
+++ lib/person.rb
@@ -11,6 +11,16 @@ class Person
         
     end
 
+    #setting our bank_account 
+    def bank_account=(value)
+        @bank_account = value
+    end
+
+    #getting our bank_account 
+    def bank_account
+        @bank_account
+    end
+
   
 
 end
@@ -20,10 +30,10 @@ human = Person.new("Wanjiku")
 puts human.name
 
 #calling our instance viable bank_account  currently initialized to 25
-puts human.bank_account
+puts human.bank_account=56
 
 #calling our instace variable happiness currently initialized  to 8
-puts human.happiness
+# puts human.happiness
 
 
 
